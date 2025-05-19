b047082a26e8a559b0f5eaec6b7f27ef1076bd4d
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-05-19 21:12:54 +0530

    add screenshots

--- day4/src/main/java/lk/ac/vau/fas/ict/Day4Application.java
+++ day4/src/main/java/lk/ac/vau/fas/ict/Day4Application.java
@@ -2,8 +2,10 @@ package lk.ac.vau.fas.ict;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class Day4Application {
 
 	public static void main(String[] args) {
